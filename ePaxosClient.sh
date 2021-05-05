1be8c6e0fcadc8b91f8dcb667175908b9f429649
root <root@tianshu-server-b4.c.vnf-store.internal>
2021-05-05 20:29:42 +0000

    prepare EPaxos test

---
+++ ePaxosClient.sh
@@ -0,0 +1,10 @@
+NClient=100
+NReq=1000
+clientBatchSize=10
+rounds=$((NReq / clientBatchSize))
+
+for((c=1; c<=$NClient; c++))
+do
+  bin/client -maddr 10.142.15.203 -q $NReq -e=true -r $rounds & 
+#> logs/C-$c.out 2>&1 &
+done 
