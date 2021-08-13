b400a6a304261e4675404d79c36908a0c8ffcfc5
Neo Zhou <zhouaea@bc.edu>
2021-08-13 14:14:13 -0400

    test clock = 5 microseconds

--- src/epaxos/epaxos.go
+++ src/epaxos/epaxos.go
@@ -232,7 +232,7 @@ var slowClockChan chan bool
 
 func (r *Replica) fastClock() {
 	for !r.Shutdown {
-		time.Sleep(1e2 * 50) // 500 nanoseconds
+		time.Sleep(1e3 * 50) // 5 microseconds
 		fastClockChan <- true
 	}
 }
